7a80b42ab43edb29688a64a6945c3d6df42b085d
ke wangjing <kewangjing@svd-4jj26t6ww.bcg.com>
2024-05-09 00:24:36 -0700

    deviation agent and sql tool

--- scripts/sql_db_tool/2-load_deviations.sql
+++ scripts/sql_db_tool/2-load_deviations.sql
@@ -1,5 +1,5 @@
--- Create table
-CREATE TABLE COMMITS (
+-- Create table under 'public' database
+CREATE TABLE deviations (
     id TEXT,
     recurring_deviation TEXT,
     desc_en TEXT,
@@ -9,6 +9,6 @@ CREATE TABLE COMMITS (
 );
 
 -- Copy CSV
-COPY COMMITS (id, recurring_deviation, desc_en, justification_en, summary_en, cause_en)
+COPY deviations (id, recurring_deviation, desc_en, justification_en, summary_en, cause_en)
 FROM '/docker-entrypoint-initdb.d/translated_deviation_data_sample_agentkit.csv'
 WITH CSV HEADER;
